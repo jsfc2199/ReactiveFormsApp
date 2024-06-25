7d4b68d5c428869db193b710f2d704537c83e94f
Juanse Franco <jsfc2199@gmail.com>
2024-06-25 17:11:03 -0500

    Se implementa formulario reactivo de dos formas con formGroup y formBuilder

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  //primera forma
+  myForm: FormGroup = new FormGroup({
+    name: new FormControl('', [], []),
+    price: new FormControl(0, [], []),
+    inStorage: new FormControl(0, [], []), // argumentos son valor por defecto, validaciones síncronas, validaciones asíncronas
+  })
+
+  //segunda forma usando FormBuilder
+  constructor(private fb: FormBuilder){}
+
+  //nos ahorramos escribir siempre formControl
+  myForm2 = this.fb.group({
+    name: ['', [], []],
+    price: [0, [], []], //si no tiene validaciones seria [0]
+    inStorage: [0, [], []],
+  })
+
+  onSave(){
+    console.log(this.myForm2.value);
+
+  }
 }
