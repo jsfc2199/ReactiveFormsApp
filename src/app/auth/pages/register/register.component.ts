93f96f0155422810564a8bdc134e0f7f1fa39a40
Juanse Franco <jsfc2199@gmail.com>
2024-06-26 11:25:11 -0500

    reactive forms en el register component

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+  
+  public form: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: keyof typeof this.form.controls) {
+    return (
+      this.form.controls[field].errors &&
+      this.form.controls[field].touched
+    );
+  }
+
+  onSave(){
+    this.form.markAllAsTouched();
+  }
 }
